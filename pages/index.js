e5cf414e6126628d01f4d8bbd7fdc79f5c5d26af
Afonso <eng.afonsocosta@outlook.com>
2024-11-19 01:47:05 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Diane, eu amo você. Se você me ama, dá uma risadinha (e o cuzin).</h1>
+  return (
+    <h1>Diane, eu amo você. Se você me ama, dá uma risadinha (e o cuzin).</h1>
+  );
 }
 
 export default Home;
